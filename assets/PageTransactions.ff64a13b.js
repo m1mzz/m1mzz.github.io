ba42b9a73a87c64c0e7ba3928c79f280e2f3fbb5
Roman Frolov <my.muzzz@gmail.com>
2021-11-09 13:51:34 +0200

    demo finish

--- assets/PageTransactions.ff64a13b.js
+++
@@ -1,2 +0,0 @@
-import{q as o,r as c,o as l,b as u,l as d,t,u as i,d as r}from"./vendor.fbce4e70.js";import{U as p}from"./index.fcc6e51f.js";const x={setup(f){const{params:{address:e}}=o(),{getAllTransactions:n}=p(),s=c([]);return n(e).then(a=>{s.value=a}),(a,m)=>(l(),u("div",null,[d(" address: "+t(i(e))+" ",1),r("div",null,[r("pre",null,"        "+t(s.value)+`
-      `,1)])]))}};export{x as default};
